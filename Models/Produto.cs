d8424ba370f27813fd46353be57607474f82755b
juliocesar-dev-br <jc309681@gmail.com>
2026-09-05 19:20:14 -0300

    feat: create and display product

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -5,4 +5,20 @@ class Produto
     public decimal Preco {get; private set;}
     public int Quantidade {get; private set;}
     public string Categoria {get; private set;} = string.Empty;
+
+
+    public Produto(int id, string nome, decimal preco, int quantidade, string categoria)
+    {
+        Id = id;
+        Nome = nome;
+        Preco = preco;
+        Quantidade = quantidade;
+        Categoria = categoria;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id} | Nome: {Nome} | Preço: {Preco} | Quantidade: {Quantidade} | Categoria: {Categoria}";
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿Produto produto1 = new Produto(001,"Compressor 1/4 Electrolux 220V", 333.99m, 20, "Peças");
+
+System.Console.WriteLine(produto1);
