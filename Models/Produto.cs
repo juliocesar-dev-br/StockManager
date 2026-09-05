c7a755a2e0043e2bfa4125e0a31ae7c871e1c38d
juliocesar-dev-br <jc309681@gmail.com>
2026-09-05 19:12:54 -0300

    feat: add product model

---
+++ Models/Produto.cs
@@ -0,0 +1,8 @@
+class Produto
+{
+    public int Id {get; private set;}
+    public string Nome {get; private set;} = string.Empty;
+    public decimal Preco {get; private set;}
+    public int Quantidade {get; private set;}
+    public string Categoria {get; private set;} = string.Empty;
+}
