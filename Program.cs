005a81bc21cc9c86d4dea0a0ede17a1c95eacbb3
juliocesar-dev-br <jc309681@gmail.com>
2026-09-05 22:20:49 -0300

    feat: add interactive inventory menu

--- Program.cs
+++ Program.cs
@@ -1,65 +1,84 @@
 ﻿
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
 
+bool sair = false;
 Estoque estoque = new Estoque();
-Estoque estoqueVazio = new Estoque();
 
-Produto produto1 = new Produto(1,"Compressor 1/4 Electrolux 220V", 333.99m, 20, "Peças");
-Produto produto2 = new Produto(1,"Compressor 1/5 Electrolux 220V", 319.99m, 20, "Peças");
-Produto produto3 = new Produto(2,"Compressor 1/3 Electrolux 220V", 540.99m, 20, "Peças");
+System.Console.WriteLine("==============================================================================================");
+System.Console.WriteLine("--------------------------------Gerenciamento de Estoque--------------------------------------");
+System.Console.WriteLine("==============================================================================================");
 
-System.Console.WriteLine(produto1);
 
-System.Console.WriteLine(produto1.RemoverQuantidade(-1));
-
-System.Console.WriteLine(produto1);
-
-System.Console.WriteLine(produto1.RemoverQuantidade(10));
-
-System.Console.WriteLine(produto1);
-
-System.Console.WriteLine(produto1.AdicionarQuantidade(-89));
-
-System.Console.WriteLine("-----------------------------------");
-
-Console.WriteLine(estoque.CadastrarProduto(produto1));
-Console.WriteLine(estoque.CadastrarProduto(produto2));
-Console.WriteLine(estoque.CadastrarProduto(produto3));
-
-estoque.ListarProdutos();
-System.Console.WriteLine("---------------------------------------");
-estoqueVazio.ListarProdutos();
-
-System.Console.WriteLine("----------------------------------------");
-
-Produto? produto4 = estoque.BuscarProdutoPorId(2);
-Produto? produto5 = estoque.BuscarProdutoPorId(99);
-
-if(produto4 != null)
+while(!sair)
 {
-    System.Console.WriteLine(produto4);
+    System.Console.WriteLine("1 - Cadastrar");
+    System.Console.WriteLine("2 - Listar");
+    System.Console.WriteLine("3 - Buscar");
+    System.Console.WriteLine("4 - Remover");
+    System.Console.WriteLine("5 - Sair");
+    System.Console.WriteLine("------------------------------------------------------------------------------------------");
+
+    System.Console.Write("Digite a opção: ");
+    string opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            System.Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            System.Console.Write("Preço: ");
+            decimal preco = decimal.Parse(Console.ReadLine());
+
+            System.Console.Write("Quantidade: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Categoria: ");
+            string categoria = Console.ReadLine();
+
+            Produto produto = new Produto(id, nome, preco, qtd, categoria);
+
+            estoque.CadastrarProduto(produto);
+            break;
+        case "2":
+            estoque.ListarProdutos();
+            break;
+        case "3":
+            System.Console.Write("Digite o id do produto que você deseja procurar: ");
+            int id1 = int.Parse(Console.ReadLine());
+
+            Produto? produto2 = estoque.BuscarProdutoPorId(id1);
+
+            if(produto2 != null)
+            {
+                System.Console.WriteLine("Produto encontrado!");
+            }
+            else
+            {
+                System.Console.WriteLine("Produto não encontrado!");
+            }
+
+
+            break;
+        case "4": 
+
+            System.Console.Write("Digite o id do produto que você deseja remover: ");
+            int id2 = int.Parse(Console.ReadLine());
+
+            estoque.RemoverProdutoPorId(id2);
+            break;
+        case "5":
+            sair = true;
+            break;
+        default:
+            System.Console.WriteLine("Opção inserida não é aceita!");
+            break;
+
+    }
+
+
 }
-else
-{
-    System.Console.WriteLine("Produto não encontrado!");
-}
-
-if(produto5 != null)
-{
-    System.Console.WriteLine(produto5);
-}
-else
-{
-    System.Console.WriteLine("Produto não encontrado!");
-}
-
-System.Console.WriteLine("-----------------------------------------------------------------");
-
-estoque.ListarProdutos();
-
-estoque.RemoverProdutoPorId(2);
-
-estoque.ListarProdutos();
-
-estoque.RemoverProdutoPorId(99);
-
-estoque.ListarProdutos();
